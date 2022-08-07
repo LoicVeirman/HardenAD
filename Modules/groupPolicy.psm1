a6236bf27a8de1d927608c11ce7fdb1e56b826c4
guillaume.mathieu <msreport@free.fr>
2022-08-07 14:44:13 +0000

    Solve bug with migtable

--- Modules/groupPolicy.psm1
+++ Modules/groupPolicy.psm1
@@ -7,8 +7,7 @@
 ## Version: 02.00.000                                           ##
 ##  Author: contact@hardenad.net                                ##
 ##################################################################
-Function Convert-MigrationTable
-{
+Function Convert-MigrationTable {
     <#
         .SYNPOSIS
             This function will replace the specified name in a .migTable file to the target one.
@@ -26,13 +25,13 @@ Function Convert-MigrationTable
             GPO to be translated.
 
         .NOTES
-            Version: 02.00
+            Version: 02.01
             Author.: contact@hardenad.net  - MSSEC
             Desc...: Function rewrite. Logging are no more used to ease the script analysis.
     #>
 
     Param(
-        [Parameter(mandatory=$true)]
+        [Parameter(mandatory = $true)]
         [String]
         $GpoName
     )
@@ -40,36 +39,35 @@ Function Convert-MigrationTable
     #.Testing if the GPO files are reachable and if a translated file already exist and should be deleted first.
     $gpoPath = ".\Inputs\GroupPolicies\" + $GpoName
 
-    if (Test-Path $gpoPath)
-    {
-        if (Test-Path ($gpoPath + "\translated.migtable"))
-        {
+    if (Test-Path $gpoPath) {
+        if (Test-Path ($gpoPath + "\translated.migtable")) {
             Remove-Item ($gpoPath + "\translated.migtable") -Force
             #.Double check for deletion
             Start-Sleep -Milliseconds 10
-            if (Test-Path ($gpoPath + "\translated.migtable"))
-            {
+            if (Test-Path ($gpoPath + "\translated.migtable")) {
                 #.Deletion KO
                 $resultCode = $false
                 $resultat = 1
-            } Else {
+            }
+            Else {
                 #.Deletion OK
                 $resultCode = $true
                 $resultat = 0
             }
-        } Else {
+        }
+        Else {
             #.No prerun file
             $resultCode = $true
             $resultat = 0
         }
         #.Ensuring a migtable file is present
-        if (-not(Test-Path ($gpoPath + "\hardenad.migtable")))
-        {
+        if (-not(Test-Path ($gpoPath + "\hardenad.migtable"))) {
             #.Not needed.
             $resultCode = $false
             $resultat = 0
         }
-    } else {
+    }
+    else {
         #.Data missing
         $resultCode = $false
         $resultat = 2
@@ -78,68 +76,74 @@ Function Convert-MigrationTable
     #.Once we have checked the GPO exists and there is no leaving trace of a previous run, we can start the translation.
     #.The whole translation process will refer to the TasksSequence.xml file to match a source ID to its target: a target is refered 
     #.as a variable stored as %xxxx% - this is the value you should find in the translation.XML file. 
-    if ($resultCode)
-    {
-        #.Opening the migtable file to a XML variable - if failed, the function stop.
+    if ($resultCode) {
+        # Opening the migtable file to a text format - if failed, the function stop.
         $xmlData = Get-Content ($gpoPath + "\hardenad.migtable") -ErrorAction Stop
 
         #.Opening the xml data from the tasks sequence for translation then filtering to the needed data
         $xmlRefs = ([xml](Get-Content .\Configs\TasksSequence_HardenAD.xml -ErrorAction Stop)).Settings.Translation.wellKnownID
-        $xmlObjs = ([xml](Get-Content $gpoPath\translation.xml -ErrorAction Stop)).translation.migTable.replace
+        
+        # Opening the migtable file to a XML format - if failed, the function stop.
+        $xmlObjs = ([xml](Get-Content ($gpoPath + "\hardenad.migtable") -ErrorAction Stop)).MigrationTable.mapping
 
-        #.Translating migration table
+        #.Translating migration
+        # Update each entry of the Harden.migtable file (XML file)
         foreach ($obj in $xmlObjs) {
-            $newDestination = $obj.NewDestination
-            #.Checking if referal is requiered
-            if ($NewDestination -match "%*%")
-            {
-                #. Referal requested, we will replace every occurence to its new value.
-                foreach ($ref in $xmlRefs)
-                {
-                    $newDestination = $newDestination -replace $ref.translateFrom,$ref.TranslateTo
+            $Destination = $obj.Destination
+            # Replace variable by the target value defined in the configuration file TasksSequence_HardenAD.xml
+            # This will replace domain and name of the group during multiple loop
+            if ($Destination -match "%*%") {
+                foreach ($ref in $xmlRefs) {
+                    $Destination = $Destination -replace $ref.translateFrom, $ref.TranslateTo
                 }
             }
+            #Write-Host $Destination
+            #write-host $($obj.objectClass)
 
-            #.Translating
-            switch ($obj.Type)
-            {
-                "User"           { 
-                                    Try { 
-                                            $xmlData = $xmlData -replace $obj.Destination,(Get-AdUser $NewDestination -ErrorAction stop).SID 
-                                    } Catch {
-                                        #.No replace
-                                    }
-                                 }
-                "Computer"       { 
-                                    Try {
-                                            $xmlData = $xmlData -replace $obj.Destination,(Get-AdComputer $NewDestination -ErrorAction stop).SID 
-                                    } Catch {
-                                        #.No replace
-                                    }
-                                 }
-                "LocalGroup"     { 
-                                    Try {
-                                            $xmlData = $xmlData -replace $obj.Destination,(Get-AdGroup $NewDestination -ErrorAction stop).SID 
-                                    } Catch {
-                                        #.No replace
-                                    }
-                                 }
-                "GlobalGroup"    { 
-                                    Try {
-                                            $xmlData = $xmlData -replace $obj.Destination,(Get-AdGroup $NewDestination -ErrorAction stop).SID 
-                                    } Catch {
-                                        #.Noreplace
-                                    }
-                                 }
+            # Generate the translated.migtable file (result file) 
+            switch ($obj.Type) {
+                "User" { 
+                    Try { 
+                        $xmlData = $xmlData -replace $obj.Destination, $Destination 
+                    }
+                    Catch {
+                        #.No replace
+                    }
+                }
+                "Computer" { 
+                    Try {
+                        $xmlData = $xmlData -replace $obj.Destination, $Destination 
+                    }
+                    Catch {
+                        #.No replace
+                    }
+                }
+                "LocalGroup" { 
+                    Try {
+                        $xmlData = $xmlData -replace $obj.Destination, $Destination
+                    }
+                    Catch {
+                        #.No replace
+                    }
+                }
+                "GlobalGroup" { 
+                    Try {
+                        $xmlData = $xmlData -replace $obj.Destination, $Destination
+                    }
+                    Catch {
+                        #.Noreplace
+                    }
+                }
                 "UniversalGroup" { 
-                                    Try {
-                                            $xmlData = $xmlData -replace $obj.Destination,(Get-AdGroup $NewDestination).SID 
-                                    } Catch {
-                                        #.No replace
-                                    }
-                                 }
-                "UNCPath"        { $xmlData = $xmlData -replace $obj.Destination,$newDestination }
-                "Unknown"        { $xmlData = $xmlData -replace $obj.Destination,$NewDestination }
+                    Try {
+                        $xmlData = $xmlData -replace $obj.Destination, $Destination
+                    }
+                    Catch {
+                        #.No replace
+                    }
+                }
+                "UNCPath" { $xmlData = $xmlData -replace $obj.Destination, $Destination }
+                "Unknown" { $xmlData = $xmlData -replace $obj.Destination, $Destination }
             }
         }
 
