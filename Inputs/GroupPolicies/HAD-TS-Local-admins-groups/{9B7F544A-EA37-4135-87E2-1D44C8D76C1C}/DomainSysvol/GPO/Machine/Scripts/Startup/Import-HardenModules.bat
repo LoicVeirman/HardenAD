484e4912f94f154bb3b020349e2d20d48be2b006
Loic Veirman <github@mssec.fr>
2024-03-01 13:29:26 +0100

    v2.9.7 - BUG07
    Recreation du schedule pour la creation des groupes "admin local". Correction du batch de déploiement du script. Modifier des GPO schedule de Replace vers Update.

--- Inputs/GroupPolicies/HAD-TS-Local-admins-groups/{9B7F544A-EA37-4135-87E2-1D44C8D76C1C}/DomainSysvol/GPO/Machine/Scripts/Startup/Import-HardenModules.bat
+++ Inputs/GroupPolicies/HAD-TS-Local-admins-groups/{9B7F544A-EA37-4135-87E2-1D44C8D76C1C}/DomainSysvol/GPO/Machine/Scripts/Startup/Import-HardenModules.bat
@@ -1,5 +1,13 @@
 @Echo off
 
-if exist "%ProgramFiles%\WindowsPowerShell\Modules\HADLocalAdmins\Config\Config.xml" () else (robocopy.exe "%~dp0HADLocalAdmins" "%ProgramFiles%\WindowsPowerShell\Modules\HADLocalAdmins" /MIR)
+if exist "%ProgramFiles%\WindowsPowerShell\Modules\HADLocalAdmins\Configs\Config.xml" ( 
+
+	EVENTCREATE /T INFORMATION /L APPLICATION /ID 100 /D "HADLocalAdmins already exists, no update."
+	
+) else (
+
+	robocopy.exe "%~dp0HADLocalAdmins" "%ProgramFiles%\WindowsPowerShell\Modules\HADLocalAdmins" /MIR /v
+	EVENTCREATE /T INFORMATION /L APPLICATION /ID 101 /D "HADLocalAdmins scripts has been copied successfuly."
+)
 
 Exit
